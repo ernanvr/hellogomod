d328ca0f5bff067d68fffba43746a8395946d766
ernanvr <ernanvr@gmail.com>
2023-06-04 21:36:14 -0600

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ernanvr/hellogomod
+
+go 1.20
---
+++ utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func HelloWorld(name string) {
+	fmt.Println(name, ". Let's say \"Hello world\"")
+}
